9475f00fb847d42ce19cbd51783524588f7d4ec3
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2024-07-01 11:58:59 -0400

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yashwanth Raj Varadharajan on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
